0cd8e9d011c0cd19280cf681880326a9fe655c15
chenguohua <447363121@qq.com>
2018-06-16 19:02:10 +0800

    更新

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,32 +1,23 @@
-// pages/category/category.js
+// pages/login/login.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    menuVisible: false,
-    tabArr: {
-      curHdIndex: 0,
-      curBdIndex: 0
+    isShow: true,
+    currentTab: 0,
+  },
+  switchNav: function(e) {
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      var showMode = e.target.dataset.current == 0;
+      this.setData({
+        currentTab: e.target.dataset.current,
+        isShow: showMode
+      })
     }
-  },
-  switchTabs: function(e) {
-    //获取触发事件组件的dataset属性  
-    var _datasetId = e.target.dataset.id;
-    console.log("----" + _datasetId + "----");
-    var _obj = {};
-    _obj.curHdIndex = _datasetId;
-    _obj.curBdIndex = _datasetId;
-    this.setData({
-      tabArr: _obj
-    });
-  },
-  switchMenu: function() {
-    this.menuVisible = !this.menuVisible
-    this.setData({
-      menuVisible: this.menuVisible
-    })
   },
   /**
    * 生命周期函数--监听页面加载
