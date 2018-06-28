60762a2c79c05797e12fce32211a45cf2a7110cc
chenguohua <447363121@qq.com>
2018-06-28 09:38:36 +0800

    更新

--- pages/category/category.js
+++ pages/category/category.js
@@ -380,13 +380,14 @@ Page({
   switchTabs: function(e) {
     //获取触发事件组件的dataset属性  
     var _datasetId = e.target.dataset.id;
-    console.log("----" + _datasetId + "----");
-    var _obj = {};
-    _obj.curHdIndex = _datasetId;
-    _obj.curBdIndex = _datasetId;
-    this.setData({
-      tabArr: _obj
-    });
+    if (_datasetId) {
+      var _obj = {};
+      _obj.curHdIndex = _datasetId;
+      _obj.curBdIndex = _datasetId;
+      this.setData({
+        tabArr: _obj
+      });
+    }
   },
   switchMenu: function() {
     this.menuVisible = !this.menuVisible
